b6525e241691e8b4bbb17c413604a6b9f6c27bb1
Никита <click.95@mail.ru>
2022-03-31 12:46:19 +0700

    Add IObservable Implementation.

---
+++ Subscriber.cs
@@ -0,0 +1,20 @@
+﻿namespace Untyped;
+public class Subscriber<T> : IObservable<T>
+{
+    private readonly List<IObserver<T>> _observers;
+
+    internal Subscriber(List<IObserver<T>> list)
+    {
+        _observers = list;
+    }
+
+    public IDisposable Subscribe(IObserver<T> observer)
+    {
+        if (!_observers.Contains(observer))
+        {
+            _observers.Add(observer);
+        }
+
+        return new Unsubscriber<T>(_observers, observer);
+    }
+}
--- UntypedCollection.cs
+++ UntypedCollection.cs
@@ -1,82 +1,102 @@
-﻿namespace Untyped
+﻿namespace Untyped;
+
+public class UntypedCollection
 {
-    public class UntypedCollection
+    private readonly Dictionary<Type, object> _collections = new();
+    private readonly Dictionary<Type, object> _idSelectors = new();
+    private readonly Dictionary<Type, object> _observers = new();
+
+    public void Add<T>(T item)
     {
-        private readonly Dictionary<Type, object> _collections = new();
-        private readonly Dictionary<Type, object> _idSelectors = new();
+        Type type = typeof(T);
 
-        public void Add<T>(T item)
+        if (!_collections.ContainsKey(type))
         {
-            Type type = typeof(T);
-
-            if (!_collections.ContainsKey(type))
-            {
-                _collections.Add(type, new List<T>());
-            }
-
-            (_collections[type] as List<T>)!.Add(item);
+            _collections.Add(type, new List<T>());
         }
 
-        public IEnumerable<T> Get<T>()
+        (_collections[type] as List<T>)!.Add(item);
+
+        if (!_observers.ContainsKey(type))
         {
-            Type type = typeof(T);
-            return _collections.ContainsKey(type) ? (List<T>)_collections[type]
-                : throw new InvalidOperationException($"No collection for {type.Name}");
+            return;
         }
 
-        public T Get<T, K>(K id)
+        foreach (var observer in (_observers[type] as List<IObserver<T>>)!)
         {
-            Type type = typeof(T);
-
-            if (!_collections.ContainsKey(type))
-            {
-                throw new InvalidOperationException($"No collection for {type.Name}");
-            }
-            if (!_idSelectors.TryGetValue(type, out var selector))
-            {
-                throw new InvalidOperationException($"No selector for {type.Name}");
-            }
-
-            Func<T, K> typedSelector;
-            try
-            {
-                typedSelector = (Func<T, K>)selector;
-            }
-            catch(InvalidCastException)
-            {
-                throw new InvalidOperationException($"No selector for {type.Name} with id type {typeof(K).Name}");
-            }
-
-            return (_collections[type] as List<T>)!.First(storedItem => typedSelector(storedItem)!.Equals(id));
+            observer.OnNext(item);
         }
-        public bool Remove<T>(T item)
+    }
+
+    public IEnumerable<T> Get<T>()
+    {
+        Type type = typeof(T);
+        return _collections.ContainsKey(type) ? (List<T>)_collections[type]
+            : throw new InvalidOperationException($"No collection for {type.Name}");
+    }
+
+    public T Get<T, K>(K id)
+    {
+        Type type = typeof(T);
+
+        if (!_collections.ContainsKey(type))
         {
-            return (_collections[typeof(T)] as List<T>)!.Remove(item);
+            throw new InvalidOperationException($"No collection for {type.Name}");
+        }
+        if (!_idSelectors.TryGetValue(type, out var selector))
+        {
+            throw new InvalidOperationException($"No selector for {type.Name}");
         }
 
-        public bool Remove<T, K>(K id)
+        Func<T, K> typedSelector;
+        try
         {
-            Type type = typeof(T);
-
-            if (!_collections.ContainsKey(type))
-            {
-                throw new InvalidOperationException($"No collection for {type.Name}");
-            }
-            if (!_idSelectors.TryGetValue(type, out var selector))
-            {
-                throw new InvalidOperationException($"No selector for {type.Name}");
-            }
-
-            var item = (_collections[type] as List<T>)!.First(storedItem => (selector as Func<T, K>)!(storedItem)!.Equals(id));
-
-            return (_collections[type] as List<T>)!.Remove(item);
+            typedSelector = (Func<T, K>)selector;
+        }
+        catch(InvalidCastException)
+        {
+            throw new InvalidOperationException($"No selector for {type.Name} with id type {typeof(K).Name}");
         }
 
-        public UntypedCollection SetId<T, K>(Func<T, K> selector)
-        {
-            _idSelectors[typeof(T)] = selector;
+        return (_collections[type] as List<T>)!.First(storedItem => typedSelector(storedItem)!.Equals(id));
+    }
+    public bool Remove<T>(T item)
+    {
+        return (_collections[typeof(T)] as List<T>)!.Remove(item);
+    }
 
-            return this;
+    public bool Remove<T, K>(K id)
+    {
+        Type type = typeof(T);
+
+        if (!_collections.ContainsKey(type))
+        {
+            throw new InvalidOperationException($"No collection for {type.Name}");
         }
+        if (!_idSelectors.TryGetValue(type, out var selector))
+        {
+            throw new InvalidOperationException($"No selector for {type.Name}");
+        }
+
+        var item = (_collections[type] as List<T>)!.First(storedItem => (selector as Func<T, K>)!(storedItem)!.Equals(id));
+
+        return (_collections[type] as List<T>)!.Remove(item);
+    }
+
+    public UntypedCollection SetId<T, K>(Func<T, K> selector)
+    {
+        _idSelectors[typeof(T)] = selector;
+
+        return this;
+    }
+
+    public Subscriber<T> GetSubscriber<T>()
+    {
+        Type type = typeof(T);
+
+        List<IObserver<T>> list = new();
+        _observers.Add(type, list);
+
+        return new Subscriber<T>(list);
     }
 }
